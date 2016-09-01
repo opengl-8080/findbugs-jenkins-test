091c9185f2cf9fa7a08e6e205f1d45c0ed6aea57
opengl-8080 <tomcat.port.8080+github@gmail.com>
2016-09-01 23:37:24 +0900

    add failure method

--- src/main/java/sample/findbugs/Main.java
+++ src/main/java/sample/findbugs/Main.java
@@ -2,7 +2,19 @@ package sample.findbugs;
 
 public class Main {
 
-    public void method() {
+    public void method1() {
+        new Exception();
+    }
+
+    public void method2() {
+        new Exception();
+    }
+
+    public void method3() {
+        new Exception();
+    }
+
+    public void method4() {
         new Exception();
     }
 }
